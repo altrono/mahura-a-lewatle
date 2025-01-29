bc82577abc26c217cea2dfea2bb6684db65f4429
altrono <hbotema16@gmail.com>
2025-01-29 20:18:38 +0200

    Implements training polish3

--- src/components/marketing/about-training.tsx
+++ src/components/marketing/about-training.tsx
@@ -6,14 +6,14 @@ import { motion } from "framer-motion"
 export function AboutTraining() {
   return (
    
-            <section id="about-us" className=" max-w-7xl mx-auto bg-white  bg-[url('/about.png')] bg-cover  bg-center] ">
-              <div className="px-4 flex  flex-col md:flex-row w-full h-full py-20 md:px-20 bg-blue-950/40  min-h-[400px]">
+            <section id="about-us" className="rounded-md max-w-7xl mx-auto bg-white  bg-[url('/about.png')] bg-cover  bg-center] ">
+              <div className="rounded-md px-4 flex  flex-col md:flex-row w-full h-full py-20 md:px-20 bg-blue-950/40  min-h-[400px]">
                 <motion.div
                   initial={{ opacity: 0, y: 50 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5 }}
                   viewport={{ once: true }}
-                  className="w-full px-4 bg-blue-950/80"
+                  className="rounded-l-md w-full px-4 bg-blue-950/80"
                 >      
                   <div className=" mx-auto px-4 md:px-8 lg:px-10 py-8">
                     <h2 className="text-lightBlue text-4xl md:text-6xl font-bold mb-4 mt-8 text-center">About <span className="text-lightGreen">Us</span></h2>
@@ -27,7 +27,7 @@ export function AboutTraining() {
                   whileInView={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.9 }}
                   viewport={{ once: true }}
-                  className=" py-8 px-4 bg-blue-950/80">
+                  className=" py-8 px-4 bg-blue-950/80 rounded-r-md">
                   <p className="text-gray-200 font-medium text-xl">MALT is an institution that trains and prepares competent and skilled seafarers for the oil and gas, offshore, merchant navy and port operations in the maritime sectors.</p>
                   <p className="text-gray-200 font-medium text-xl">We impart knowledge and skills of sea-going personnel/workforce within the
 different divisions of the marine sectors,
